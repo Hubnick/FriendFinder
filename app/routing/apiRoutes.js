ca56f6fd8740698150556b01a5413e6eb43354c8
Hubnick <jacoby.nick@gmail.com>
2018-09-18 13:06:30 -0400

    herkou uppushloadlinkasdfasdf

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var friendData = require("../data/friends");
+var friendsData = require("../data/friends");
 // var waitListData = require("../data/waitinglistData");
 
 
@@ -12,30 +12,29 @@ var friendData = require("../data/friends");
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+// API GET Requests
+// Below code handles when users "visit" a page.
+// In each of the below cases when a user visits a link
+// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+// ---------------------------------------------------------------------------
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friendData);
-  });
+// app.get("/api/friends", function(req, res) {
+//   res.json(friendData);
+// });
 
-  // app.get("/api/waitlist", function(req, res) {
-  //   res.json(waitListData);
-  // });
+// app.get("/api/waitlist", function(req, res) {
+//   res.json(waitListData);
+// });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+// API POST Requests
+// Below code handles when a user submits a form and thus submits data to the server.
+// In each of the below cases, when a user submits form data (a JSON object)
+// ...the JSON is pushed to the appropriate JavaScript array
+// (ex. User fills out a reservation request... this data is then sent to the server...
+// Then the server saves the data to the tableData array)
+// ---------------------------------------------------------------------------
 
-//   app.post("/api/friends", function(req, res) {
+// app.post("/api/friends", function(req, res) {
 //     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
 //     // It will do this by sending out the value "true" have a table
 //     // req.body is available since we're using the body-parser middleware
@@ -49,15 +48,70 @@ module.exports = function(app) {
 //     }
 //   });
 
-//   // ---------------------------------------------------------------------------
-//   // I added this below code so you could clear out the table while working with the functionality.
-//   // Don"t worry about it!
+// //   // ---------------------------------------------------------------------------
+// //   // I added this below code so you could clear out the table while working with the functionality.
+// //   // Don"t worry about it!
 
-//   app.post("/api/clear", function(req, res) {
-//     // Empty out the arrays of data
-//     tableData.length = [];
-//     waitListData.length = [];
+// //   app.post("/api/clear", function(req, res) {
+// //     // Empty out the arrays of data
+// //     tableData.length = [];
+// //     waitListData.length = [];
 
-//     res.json({ ok: true });
+// //     res.json({ ok: true });
+//   })
+
+
+
+
+
+
+
+//   // Displays a single character, or returns false
+//   app.get("/api/characters/:character", function (req, res) {
+//     var chosen = req.params.character;
+
+//     console.log(chosen);
+
+//     for (var i = 0; i < characters.length; i++) {
+//       if (chosen === characters[i].routeName) {
+//         return res.json(characters[i]);
+//       }
+//     }
+
+//     return res.json(false);
 //   });
+
+//   // Create New Characters - takes in JSON input
+//   app.post("/api/characters", function (req, res) {
+//     // req.body hosts is equal to the JSON post sent from the user
+//     // This works because of our body-parser middleware
+//     var newcharacter = req.body;
+
+//     // Using a RegEx Pattern to remove spaces from newCharacter
+//     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+//     newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+//     console.log(newcharacter);
+
+//     characters.push(newcharacter);
+
+//     res.json(newcharacter);
+//   });
+
+// };
+
+
+module.exports = function (app) {
+
+  // Displays all characters
+
+  app.get("/api/friends", function (req, res) {
+    res.json(friendsData)
+  });
+
+
+  app.post("/api/friends", function (req, res) {
+    res.json(req.body)
+  });
+
 };
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+// Dependencies
+// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-
+// Sets up the Express App
+// =============================================================
 var app = express()
-
 var PORT = process.env.PORT || 9000;
+
+// Sets up the Express app to handle data parsing
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,6 +17,10 @@ app.use(bodyParser.json());
 // app.get("/", function(req, res) {
 //   res.sendFile(path.join(__dirname, "/app/public/home.html"));
 // });
+
+// (DATA)
+// =============================================================
+
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
