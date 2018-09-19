339230233a53cc4cc382c8bd4fb19049482c3a28
Hubnick <jacoby.nick@gmail.com>
2018-09-19 18:15:39 -0400

    best commit so far

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,535 +1,533 @@
 // <!-- referenced from source -->
-
 var friendsArray = 
 [
     {
     "name": "Ahmed",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
     "scores": [
-    "5",
-    "1",
-    "4",
-    "4",
-    "5",
-    "1",
-    "2",
-    "5",
-    "4",
-    "1"
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
     ]
     },
     {
     "name": "Jacob Deming",
     "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
     "scores": [
-    "4",
-    "2",
-    "5",
-    "1",
-    "3",
-    "2",
-    "2",
-    "1",
-    "3",
-    "2"
+    4,
+    2,
+    5,
+    1,
+    3,
+    2,
+    2,
+    1,
+    3,
+    2
     ]
     },
     {
     "name": "Jeremiah Scanlon",
     "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
     "scores": [
-    "5",
-    "2",
-    "2",
-    "2",
-    "4",
-    "1",
-    "3",
-    "2",
-    "5",
-    "5"
+    5,
+    2,
+    2,
+    2,
+    4,
+    1,
+    3,
+    2,
+    5,
+    5
     ]
     },
     {
     "name": "Louis T. Delia",
     "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
     "scores": [
-    "3",
-    "3",
-    "4",
-    "2",
-    "2",
-    "1",
-    "3",
-    "2",
-    "2",
-    "3"
+    3,
+    3,
+    4,
+    2,
+    2,
+    1,
+    3,
+    2,
+    2,
+    3
     ]
     },
     {
     "name": "Lou Ritter",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
     "scores": [
-    "4",
-    "3",
-    "4",
-    "1",
-    "5",
-    "2",
-    "5",
-    "3",
-    "1",
-    "4"
+    4,
+    3,
+    4,
+    1,
+    5,
+    2,
+    5,
+    3,
+    1,
+    4
     ]
     },
     {
     "name": "Jordan Biason",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
     "scores": [
-    "4",
-    "4",
-    "2",
-    "3",
-    "2",
-    "2",
-    "3",
-    "2",
-    "4",
-    "5"
+    4,
+    4,
+    2,
+    3,
+    2,
+    2,
+    3,
+    2,
+    4,
+    5
     ]
     },
     {
     "name": "nomin",
     "photo": "/app/pic",
     "scores": [
-    "4",
-    "2",
-    "4",
-    "2",
-    "3",
-    "2",
-    "4",
-    "4",
-    "2",
-    "2"
+    4,
+    2,
+    4,
+    2,
+    3,
+    2,
+    4,
+    4,
+    2,
+    2
     ]
     },
     {
     "name": "Mary",
     "photo": "image.jpg",
     "scores": [
-    "5",
-    "3",
-    "5",
-    "3",
-    "4",
-    "1",
-    "3",
-    "3",
-    "3",
-    "3"
+    5,
+    3,
+    5,
+    3,
+    4,
+    1,
+    3,
+    3,
+    3,
+    3
     ]
     },
     {
     "name": "Garfield",
     "photo": "https://www.pinterest.com/pin/438678819936604870/",
     "scores": [
-    "1",
-    "5",
-    "4",
-    "4",
-    "4",
-    "4",
-    "2",
-    "2",
-    "3",
-    "2"
+    1,
+    5,
+    4,
+    4,
+    4,
+    4,
+    2,
+    2,
+    3,
     ]
     },
     {
     "name": "blah",
     "photo": "fsdsfa",
     "scores": [
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1"
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1
     ]
     },
     {
     "name": "blah",
     "photo": "fsdsfa",
     "scores": [
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1"
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1
     ]
     },
     {
     "name": "blah",
     "photo": "fsdsfa",
     "scores": [
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1"
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1
     ]
     },
     {
-    "name": "2",
-    "photo": "2",
+    "name": 2,
+    "photo": 2,
     "scores": [
-    "3",
-    "1",
-    "1",
-    "1",
-    "1",
-    "2",
-    "1",
-    "3",
-    "1",
-    "1"
+    3,
+    1,
+    1,
+    1,
+    1,
+    2,
+    1,
+    3,
+    1,
+    1
     ]
     },
     {
     "name": "jesselikes",
     "photo": "boobs",
     "scores": [
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5"
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5
     ]
     },
     {
     "name": "Bob",
     "photo": "C:\\bob.txt",
     "scores": [
-    "5",
-    "5",
-    "1",
-    "5",
-    "1",
-    "1",
-    "5",
-    "1",
-    "1",
-    "1"
+    5,
+    5,
+    1,
+    5,
+    1,
+    1,
+    5,
+    1,
+    1,
     ]
     },
     {
     "name": "Nomin",
     "photo": "/app/friends",
     "scores": [
-    "3",
-    "2",
-    "2",
-    "2",
-    "3",
-    "3",
-    "2",
-    "3",
-    "3",
-    "1"
+    3,
+    2,
+    2,
+    2,
+    3,
+    3,
+    2,
+    3,
+    3,
+    1
     ]
     },
     {
     "name": "Nomin",
     "photo": "/app/friends",
     "scores": [
-    "3",
-    "2",
-    "2",
-    "2",
-    "3",
-    "3",
-    "2",
-    "3",
-    "3",
-    "1"
+    3,
+    2,
+    2,
+    2,
+    3,
+    3,
+    2,
+    3,
+    3,
+    1
     ]
     },
     {
     "name": "Nomin b",
     "photo": "/app/friends",
     "scores": [
-    "3",
-    "2",
-    "2",
-    "2",
-    "3",
-    "3",
-    "2",
-    "3",
-    "3",
-    "1"
+    3,
+    2,
+    2,
+    2,
+    3,
+    3,
+    2,
+    3,
+    3,
+    1
     ]
     },
     {
     "name": "Tom",
     "photo": "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "scores": [
-    "4",
-    "3",
-    "5",
-    "3",
-    "3",
-    "3",
-    "2",
-    "3",
-    "3",
-    "2"
+    4,
+    3,
+    5,
+    3,
+    3,
+    3,
+    2,
+    3,
+    3,
+    2
     ]
     },
     {
     "name": "Tom",
     "photo": "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "scores": [
-    "4",
-    "3",
-    "5",
-    "3",
-    "3",
-    "3",
-    "2",
-    "3",
-    "3",
-    "2"
+    4,
+    3,
+    5,
+    3,
+    3,
+    3,
+    2,
+    3,
+    3,
+    2
     ]
     },
     {
     "name": "Tom",
     "photo": "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "scores": [
-    "4",
-    "3",
-    "5",
-    "3",
-    "3",
-    "3",
-    "2",
-    "3",
-    "3",
-    "2"
+    4,
+    3,
+    5,
+    3,
+    3,
+    3,
+    2,
+    3,
+    3,
+    2
+    
+    
     ]
     },
     {
     "name": "Tom",
     "photo": "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "scores": [
-    "4",
-    "3",
-    "5",
-    "3",
-    "3",
-    "3",
-    "2",
-    "3",
-    "3",
-    "2"
+    4,
+    3,
+    5,
+    3,
+    3,
+    3,
+    2,
+    3,
+    3,
+    2
     ]
     },
     {
     "name": "Tom",
     "photo": "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "scores": [
-    "4",
-    "3",
-    "5",
-    "3",
-    "3",
-    "3",
-    "2",
-    "3",
-    "3",
-    "2"
+    4,
+    3,
+    5,
+    3,
+    3,
+    3,
+    2,
+    3,
+    3,
+    2
     ]
     },
     {
     "name": "steve",
     "photo": "adsfadf",
     "scores": [
-    "5",
-    "2",
-    "2",
-    "2",
-    "4",
-    "2",
-    "5",
-    "1",
-    "2",
-    "2"
+    5,
+    2,
+    2,
+    2,
+    4,
+    2,
+    5,
+    1,
+    2,
+    2
     ]
     },
     {
     "name": "Paul",
     "photo": "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "scores": [
-    "3",
-    "4",
-    "4",
-    "3",
-    "2",
-    "2",
-    "3",
-    "1",
-    "2",
-    "1"
+    3,
+    4,
+    4,
+    3,
+    2,
+    2,
+    3,
+    1,
+    2,
+    1
     ]
     },
     {
     "name": "Paul",
     "photo": "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "scores": [
-    "3",
-    "4",
-    "4",
-    "3",
-    "2",
-    "2",
-    "3",
-    "1",
-    "2",
-    "1"
+    3,
+    4,
+    4,
+    3,
+    2,
+    2,
+    3,
+    1,
+    2,
+    1
     ]
     },
     {
     "name": "Peter Phu",
     "photo": "www.peter.jpg",
     "scores": [
-    "3",
-    "3",
-    "2",
-    "2",
-    "2",
-    "3",
-    "2",
-    "3",
-    "4",
-    "3"
+    3,
+    3,
+    2,
+    2,
+    2,
+    3,
+    2,
+    3,
+    4,
+    3
     ]
     },
     {
     "name": "Peter Phu",
     "photo": "www.peter.jpg",
     "scores": [
-    "3",
-    "3",
-    "2",
-    "2",
-    "2",
-    "3",
-    "2",
-    "3",
-    "4",
-    "3"
+    3,
+    3,
+    2,
+    2,
+    2,
+    3,
+    2,
+    3,
+    4,
+    3
     ]
     },
     {
     "name": "Peter Phu",
     "photo": "www.peter.jpg",
     "scores": [
-    "3",
-    "3",
-    "2",
-    "2",
-    "2",
-    "3",
-    "2",
-    "3",
-    "4",
-    "3"
+    3,
+    3,
+    2,
+    2,
+    2,
+    3,
+    2,
+    3,
+    4,
+    3
     ]
     },
     {
     "name": "aspodfasdf",
     "photo": "asldfnlsdkfn",
     "scores": [
-    "2",
-    "5",
-    "3",
-    "3",
-    "3",
-    "2",
-    "2",
-    "4",
-    "3",
-    "3"
+    2,
+    5,
+    3,
+    3,
+    3,
+    2,
+    2,
+    4,
+    3,
+    3
     ]
     },
     {
     "name": "Matty S Chen",
     "photo": "www.yahoo.com",
     "scores": [
-    "2",
-    "3",
-    "3",
-    "2",
-    "2",
-    "2",
-    "3",
-    "2",
-    "2",
-    "2"
+    2,
+    3,
+    3,
+    2,
+    2,
+    2,
+    3,
+    2,
+    2,
+    2
     ]
     },
     {
     "name": "Matty S Chen",
     "photo": "www.yahoo.com",
     "scores": [
-    "2",
-    "3",
-    "3",
-    "2",
-    "2",
-    "2",
-    "3",
-    "2",
-    "2",
-    "2"
+    2,
+    3,
+    3,
+    2,
+    2,
+    2,
+    3,
+    2,
+    2,
+    2
     ]
     },
     {
     "name": "aspodfasdf",
     "photo": "asldfnlsdkfn",
     "scores": [
-    "2",
-    "5",
-    "3",
-    "3",
-    "3",
-    "2",
-    "2",
-    "4",
-    "3",
-    "3"
+    2,
+    5,
+    3,
+    3,
+    3,
+    2,
+    2,
+    4,
+    3,
+    3
     ]
     }
     ]
-
     module.exports = friendsArray
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,104 +1,5 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
 
 var friendsData = require("../data/friends");
-// var waitListData = require("../data/waitinglistData");
-
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
-// API GET Requests
-// Below code handles when users "visit" a page.
-// In each of the below cases when a user visits a link
-// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-// ---------------------------------------------------------------------------
-
-// app.get("/api/friends", function(req, res) {
-//   res.json(friendData);
-// });
-
-// app.get("/api/waitlist", function(req, res) {
-//   res.json(waitListData);
-// });
-
-// API POST Requests
-// Below code handles when a user submits a form and thus submits data to the server.
-// In each of the below cases, when a user submits form data (a JSON object)
-// ...the JSON is pushed to the appropriate JavaScript array
-// (ex. User fills out a reservation request... this data is then sent to the server...
-// Then the server saves the data to the tableData array)
-// ---------------------------------------------------------------------------
-
-// app.post("/api/friends", function(req, res) {
-//     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-//     // It will do this by sending out the value "true" have a table
-//     // req.body is available since we're using the body-parser middleware
-//     if (friendData.length < 5) {
-//       tableData.push(req.body);
-//       res.json(true);
-//     }
-//     else {
-//       waitListData.push(req.body);
-//       res.json(false);
-//     }
-//   });
-
-// //   // ---------------------------------------------------------------------------
-// //   // I added this below code so you could clear out the table while working with the functionality.
-// //   // Don"t worry about it!
-
-// //   app.post("/api/clear", function(req, res) {
-// //     // Empty out the arrays of data
-// //     tableData.length = [];
-// //     waitListData.length = [];
-
-// //     res.json({ ok: true });
-//   })
-
-
-
-
-
-
-
-//   // Displays a single character, or returns false
-//   app.get("/api/characters/:character", function (req, res) {
-//     var chosen = req.params.character;
-
-//     console.log(chosen);
-
-//     for (var i = 0; i < characters.length; i++) {
-//       if (chosen === characters[i].routeName) {
-//         return res.json(characters[i]);
-//       }
-//     }
-
-//     return res.json(false);
-//   });
-
-//   // Create New Characters - takes in JSON input
-//   app.post("/api/characters", function (req, res) {
-//     // req.body hosts is equal to the JSON post sent from the user
-//     // This works because of our body-parser middleware
-//     var newcharacter = req.body;
-
-//     // Using a RegEx Pattern to remove spaces from newCharacter
-//     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-//     newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-
-//     console.log(newcharacter);
-
-//     characters.push(newcharacter);
-
-//     res.json(newcharacter);
-//   });
-
-// };
 
 
 module.exports = function (app) {
@@ -111,7 +12,34 @@ module.exports = function (app) {
 
 
   app.post("/api/friends", function (req, res) {
-    res.json(req.body)
-  });
+
+
+    //  * Convert each user's results into a simple array of numbers (ex: `["5", 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
+
+    // parsedScores = [];
+
+    // for (i = 0; i < req.scores.length; i++) {
+    //   var n = parseInt(req.scores[i]);
+    //   parsedScores.push(n);
+    // }
+
+    // function findTotal(data) {
+    //   total = "";
+    //   for (i = 0; i < data.length; i++) {
+    //     total += data[i]
+    //   }
+
+
+
+      //  * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
+
+      //    * Example: 
+      //      * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
+      //      * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
+      //      * Total Difference: **2 + 1 + 2 =** **_5_**
+      //  * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on. 
+      //  * The closest match will be the user with the least amount of difference.
+      res.json(friendsData)
+    });
 
 };
